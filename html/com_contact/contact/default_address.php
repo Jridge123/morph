92367374d03ce75f54bb3c4fae155649ae912816
Chris Rault <Chris@chris.local>
2008-09-24 09:58:43 +0200

    contact page overrides.

--- html/com_contact/contact/default_address.php
+++ html/com_contact/contact/default_address.php
@@ -2,122 +2,75 @@
 /** $Id: default_address.php 12387 2009-06-30 01:17:44Z ian $ */
 defined( '_JEXEC' ) or die( 'Restricted access' );
 ?>
-<?php if ( ( $this->contact->params->get( 'address_check' ) > 0 ) &&  ( $this->contact->address || $this->contact->suburb  || $this->contact->state || $this->contact->country || $this->contact->postcode ) ) : ?>
-<table width="100%" cellpadding="0" cellspacing="0" border="0">
-<?php if ( $this->contact->params->get( 'address_check' ) > 0 ) : ?>
-<tr>
-	<td rowspan="6" valign="top" width="<?php echo $this->contact->params->get( 'column_width' ); ?>" >
-		<?php echo $this->contact->params->get( 'marker_address' ); ?>
-	</td>
-</tr>
-<?php endif; ?>
-<?php if ( $this->contact->address && $this->contact->params->get( 'show_street_address' ) ) : ?>
-<tr>
-	<td valign="top">
-		<?php echo nl2br($this->escape($this->contact->address)); ?>
-	</td>
-</tr>
-<?php endif; ?>
-<?php if ( $this->contact->suburb && $this->contact->params->get( 'show_suburb' ) ) : ?>
-<tr>
-	<td valign="top">
-		<?php echo $this->escape($this->contact->suburb); ?>
-	</td>
-</tr>
-<?php endif; ?>
-<?php if ( $this->contact->state && $this->contact->params->get( 'show_state' ) ) : ?>
-<tr>
-	<td valign="top">
-		<?php echo $this->escape($this->contact->state); ?>
-	</td>
-</tr>
-<?php endif; ?>
-<?php if ( $this->contact->postcode && $this->contact->params->get( 'show_postcode' ) ) : ?>
-<tr>
-	<td valign="top">
-		<?php echo $this->escape($this->contact->postcode); ?>
-	</td>
-</tr>
-<?php endif; ?>
-<?php if ( $this->contact->country && $this->contact->params->get( 'show_country' ) ) : ?>
-<tr>
-	<td valign="top">
-		<?php echo $this->escape($this->contact->country); ?>
-	</td>
-</tr>
-<?php endif; ?>
-</table>
-<br />
-<?php endif; ?>
-<?php if ( ($this->contact->email_to && $this->contact->params->get( 'show_email' )) || 
-			($this->contact->telephone && $this->contact->params->get( 'show_telephone' )) || 
-			($this->contact->fax && $this->contact->params->get( 'show_fax' )) || 
-			($this->contact->mobile && $this->contact->params->get( 'show_mobile' )) || 
-			($this->contact->webpage && $this->contact->params->get( 'show_webpage' )) ) : ?>
-<table width="100%" cellpadding="0" cellspacing="0" border="0">
-<?php if ( $this->contact->email_to && $this->contact->params->get( 'show_email' ) ) : ?>
-<tr>
-	<td width="<?php echo $this->contact->params->get( 'column_width' ); ?>" >
-		<?php echo $this->contact->params->get( 'marker_email' ); ?>
-	</td>
-	<td>
-		<?php echo $this->contact->email_to; ?>
-	</td>
-</tr>
-<?php endif; ?>
-<?php if ( $this->contact->telephone && $this->contact->params->get( 'show_telephone' ) ) : ?>
-<tr>
-	<td width="<?php echo $this->contact->params->get( 'column_width' ); ?>" >
-		<?php echo $this->contact->params->get( 'marker_telephone' ); ?>
-	</td>
-	<td>
-		<?php echo nl2br($this->escape($this->contact->telephone)); ?>
-	</td>
-</tr>
-<?php endif; ?>
-<?php if ( $this->contact->fax && $this->contact->params->get( 'show_fax' ) ) : ?>
-<tr>
-	<td width="<?php echo $this->contact->params->get( 'column_width' ); ?>" >
-		<?php echo $this->contact->params->get( 'marker_fax' ); ?>
-	</td>
-	<td>
-		<?php echo nl2br($this->escape($this->contact->fax)); ?>
-	</td>
-</tr>
-<?php endif; ?>
-<?php if ( $this->contact->mobile && $this->contact->params->get( 'show_mobile' ) ) :?>
-<tr>
-	<td width="<?php echo $this->contact->params->get( 'column_width' ); ?>" >
-	<?php echo $this->contact->params->get( 'marker_mobile' ); ?>
-	</td>
-	<td>
-		<?php echo nl2br($this->escape($this->contact->mobile)); ?>
-	</td>
-</tr>
-<?php endif; ?>
-<?php if ( $this->contact->webpage && $this->contact->params->get( 'show_webpage' )) : ?>
-<tr>
-	<td width="<?php echo $this->contact->params->get( 'column_width' ); ?>" >
-	</td>
-	<td>
-		<a href="<?php echo $this->escape($this->contact->webpage); ?>" target="_blank">
-			<?php echo $this->escape($this->contact->webpage); ?></a>
-	</td>
-</tr>
-<?php endif; ?>
-</table>
-<?php endif; ?>
-<br />
-<?php if ( $this->contact->misc && $this->contact->params->get( 'show_misc' ) ) : ?>
-<table width="100%" cellpadding="0" cellspacing="0" border="0">
-<tr>
-	<td width="<?php echo $this->contact->params->get( 'column_width' ); ?>" valign="top" >
-		<?php echo $this->contact->params->get( 'marker_misc' ); ?>
-	</td>
-	<td>
-		<?php echo nl2br($this->contact->misc); ?>
-	</td>
-</tr>
-</table>
-<br />
+<?php if ( ( $this->contact->address || $this->contact->suburb  || $this->contact->state || $this->contact->country || $this->contact->postcode ) ) : ?>
+
+<div id="hcard-Chris-Rault" class="vcard">
+
+    <div class="contact">
+
+        <?php if ( $this->contact->name && $this->contact->params->get( 'show_name' ) ) : ?>
+        <h2 class="contact-name"><span class="fn"><?php echo $this->escape($this->contact->name); ?></span></h2>
+        <?php endif; ?>
+        <?php if ( $this->contact->con_position && $this->contact->params->get( 'show_position' ) ) : ?>
+        <h3 class="contact-position"><span class="role"><?php echo $this->escape($this->contact->con_position); ?></span></h3>
+        <?php endif; ?>
+     
+    </div>
+
+    <?php if ( $this->contact->address && $this->contact->params->get( 'show_street_address' ) ) : ?>
+    <div class="adr">
+        
+        <?php if ( $this->contact->address && $this->contact->params->get( 'show_street_address' ) ) : ?>
+        <span class="street-address"><?php echo nl2br($this->escape($this->contact->address)); ?></span>
+        <?php endif; ?>
+      
+        <?php if ( $this->contact->suburb && $this->contact->params->get( 'show_suburb' ) ) : ?>
+        <span class="locality"><?php echo $this->escape($this->contact->suburb); ?></span>
+        <?php endif; ?>
+        
+        <?php if ( $this->contact->state && $this->contact->params->get( 'show_state' ) ) : ?>
+        <span class="region"><?php echo $this->escape($this->contact->state); ?></span>
+        <?php endif; ?>
+        
+        <?php if ( $this->contact->postcode && $this->contact->params->get( 'show_postcode' ) ) : ?>
+        <span class="postal-code"><?php echo $this->escape($this->contact->postcode); ?></span>
+        <?php endif; ?>
+        
+        <?php if ( $this->contact->country && $this->contact->params->get( 'show_country' ) ) : ?>
+        <span class="country-name"><?php echo $this->escape($this->contact->country); ?></span>
+        <?php endif; ?>
+    
+    </div>
+    <?php endif; ?>
+ 
+    <?php if ( $this->contact->telephone && $this->contact->params->get('show_telephone') || $this->contact->fax && $this->contact->params->get('show_fax')
+      || $this->contact->mobile && $this->contact->params->get('show_mobile')) : ?>		
+    <ul class="tel">
+         <?php if ( $this->contact->telephone && $this->contact->params->get( 'show_telephone' ) ) : ?>
+        <li class="contact-tel"><span class="type">Phone</span> <?php echo nl2br($this->escape($this->contact->telephone)); ?></li>
+        <?php endif; ?>
+        
+        <?php if ( $this->contact->fax && $this->contact->params->get( 'show_fax' ) ) : ?>
+        <li class="contact-fax"><span class="type">Fax</span> <?php echo nl2br($this->escape($this->contact->fax)); ?></li>
+        <?php endif; ?>
+        
+        <?php if ( $this->contact->mobile && $this->contact->params->get( 'show_mobile' ) ) :?>
+        <li class="contact-mob"><span class="type">Mobile</span> <?php echo nl2br($this->escape($this->contact->mobile)); ?></li>
+        <?php endif; ?>
+        
+    </ul>
+    <?php endif; ?>
+
+    <?php if ( $this->contact->webpage && $this->contact->params->get( 'show_webpage' )) : ?>
+	<p class="website"><strong>Web</strong> <a href="<?php echo $this->escape($this->contact->webpage); ?>" target="_blank"><span class="org url">
+	<?php echo $this->escape($this->contact->webpage); ?></span></a></p>
+    <?php endif; ?>  
+        
+    <?php if ( $this->contact->params->get( 'allow_vcard' ) ) : ?>
+	    <p class="vcard"><?php echo JText::_( 'Download information as a' );?>
+		<a href="<?php echo JURI::base(); ?>index.php?option=com_contact&amp;task=vcard&amp;contact_id=<?php echo $this->contact->id; ?>&amp;format=raw&amp;tmpl=component">
+			<?php echo JText::_( 'VCard' );?></a>.</p>
+	<?php endif; ?>	
+	
+</div>
 <?php endif; ?>
--- html/com_contact/contact/default_form.php
+++ html/com_contact/contact/default_form.php
@@ -22,47 +22,48 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 	$document->addScriptDeclaration($script);
 	
 	if(isset($this->error)) : ?>
-<tr>
-	<td><?php echo $this->error; ?></td>
-</tr>
+<div id="message">
+<?php echo $this->error; ?>
+</div>
 <?php endif; ?>
-<tr>
-	<td colspan="2">
-	<br /><br />
+
+<div id="contact-form">
+
+<h3>Contact form:</h3>
+
+<?php if ($this->contact->params->get( 'email_description' ) !== '' ) : ?>
+<p class="form-description"><?php echo $this->contact->params->get( 'email_description' ); ?></p>
+<?php endif; ?>
+
 	<form action="<?php echo JRoute::_( 'index.php' );?>" method="post" name="emailForm" id="emailForm" class="form-validate">
-		<div class="contact_email<?php echo $this->escape($this->params->get('pageclass_sfx')); ?>">
+		<div class="contact_email">
 			<label for="contact_name">
-				&nbsp;<?php echo JText::_( 'Enter your name' );?>:
+				<?php echo JText::_( 'Enter your name' );?>:
 			</label>
-			<br />
 			<input type="text" name="name" id="contact_name" size="30" class="inputbox" value="" />
-			<br />
+
 			<label id="contact_emailmsg" for="contact_email">
-				&nbsp;<?php echo JText::_( 'Email address' );?>:
+				<?php echo JText::_( 'Email address' );?>:
 			</label>
-			<br />
 			<input type="text" id="contact_email" name="email" size="30" value="" class="inputbox required validate-email" maxlength="100" />
-			<br />
+
 			<label for="contact_subject">
-				&nbsp;<?php echo JText::_( 'Message subject' );?>:
+				<?php echo JText::_( 'Message subject' );?>:
 			</label>
-			<br />
 			<input type="text" name="subject" id="contact_subject" size="30" class="inputbox" value="" />
-			<br /><br />
+
 			<label id="contact_textmsg" for="contact_text">
-				&nbsp;<?php echo JText::_( 'Enter your message' );?>:
+				<?php echo JText::_( 'Enter your message' );?>:
 			</label>
-			<br />
+
 			<textarea cols="50" rows="10" name="text" id="contact_text" class="inputbox required"></textarea>
 			<?php if ($this->contact->params->get( 'show_email_copy' )) : ?>
-			<br />
-				<input type="checkbox" name="email_copy" id="contact_email_copy" value="1"  />
-				<label for="contact_email_copy">
-					<?php echo JText::_( 'EMAIL_A_COPY' ); ?>
-				</label>
+
+			<p><input type="checkbox" name="email_copy" id="contact_email_copy" value="1"  />
+			<label for="contact_email_copy">
+				<?php echo JText::_( 'EMAIL_A_COPY' ); ?>
+			</label></p>
 			<?php endif; ?>
-			<br />
-			<br />
 			<button class="button validate" type="submit"><?php echo JText::_('Send'); ?></button>
 		</div>
 
@@ -72,6 +73,4 @@ defined( '_JEXEC' ) or die( 'Restricted access' );
 	<input type="hidden" name="task" value="submit" />
 	<?php echo JHTML::_( 'form.token' ); ?>
 	</form>
-	<br />
-	</td>
-</tr>
+</div>
