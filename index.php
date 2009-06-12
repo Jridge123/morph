615d853e719083ab9ac54f920937006bec989c6d
Chris <chris@chris-raults-macbook-pro-152.local>
2009-06-12 11:42:59 +0200

    changed the page suffix to be a class and set the themelet as the body's id.

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ include_once(JPATH_ROOT . "/templates/" . $this->template . '/core/morphFunction
 <head>
 <jdoc:include type="head" />
 </head>
-<body class="js-disabled <?php echo $themelet ?> <?php echo "$lcbrowser $lcbrowser$ver"; ?>"<?php if ($pageclass != ""){ echo ' id="'.$pageclass.'"'; } ?>>
+<body class="js-disabled <?php echo "$lcbrowser $lcbrowser$ver"; if ($pageclass != ""){ echo ' '.$pageclass; } ?>"<?php if ($themelet != ""){ echo ' id="'.$themelet.'"'; } ?>>
 <?php if($this->countModules('advert1')) { ?><div id="advert1"><jdoc:include type="modules" name="advert1" style="none" /></div><?php } ?>
 <?php
 // toolbar + topnav after toolbar
