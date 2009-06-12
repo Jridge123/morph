5210e6e785816c5d1253274c1a86085bb37dc5bc
Chris <chris@chris-raults-macbook-pro-152.local>
2009-06-12 15:06:28 +0200

    removed the site width from being set on the user1 and user2 divs, since that is being controlled via the #bd parent.

--- core/includes/user1.php
+++ core/includes/user1.php
@@ -1,5 +1,5 @@
 <?php if($this->countModules('user1') && $user1_show == 0 ) { ?>
-<div id="user1" class="<?php echo $site_width ?> intelli <?php getYuiSuffix('user1', $jj_const); ?> <?php echo $user1_chrome ?> modcount<?php echo $user1count ?>">
+<div id="user1" class="intelli <?php getYuiSuffix('user1', $jj_const); ?> <?php echo $user1_chrome ?> modcount<?php echo $user1count ?>">
 	<?php if ( $user1_inner == "1" ) { ?><div id="user1-inner"><?php } ?>
 	<jdoc:include type="modules" name="user1" style="<?php echo $user1_chrome ?>" />
 	<?php if ( $user1_inner == "1" ) { ?></div><?php } ?>
--- core/includes/user2.php
+++ core/includes/user2.php
@@ -1,5 +1,5 @@
 <?php if($this->countModules('user2') && $user2_show == 0 ) { ?>
-<div id="user2" class="<?php echo $site_width ?> intelli <?php getYuiSuffix('user2', $jj_const); ?> <?php echo $user2_chrome ?>">
+<div id="user2" class="intelli <?php getYuiSuffix('user2', $jj_const); ?> <?php echo $user2_chrome ?>">
 	<?php if ( $user2_inner == 1 ) { ?><div id="user2-inner"><?php } ?>
 	<jdoc:include type="modules" name="user2" style="<?php echo $user2_chrome ?>" />
 	<?php if ( $user2_inner == 1 ) { ?></div><?php } ?>
