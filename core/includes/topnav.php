5f0119d76fc520548647ab643ae9b6eea2a29ba9
Chris Rault <Chris@chris.local>
2009-06-18 12:00:55 +0200

    Added the menu specific classes to the #nav.

--- core/includes/topnav.php
+++ core/includes/topnav.php
@@ -1,8 +1,8 @@
 <?php if ($this->countModules( 'user3 or user4' ) && $topnav_show == 0 ) { ?>
 <?php if ( $topnav_wrap == "1" ) { ?><div id="topnav-wrap"><?php } ?>
-	<div id="topnav" class="<?php echo $site_width; if ( $subtext_top >= 1 ) { ?> subtext<?php } if ( $topnav_actionlink >= 1 ) { ?> call-for-action<?php } if ( $topfish >= 1 ) { ?> topfish<?php } ?> clearer">
+	<div id="topnav" class="<?php echo $site_width; if ( $subtext_top >= 1 ) { ?> subtext<?php } if ( $topnav_actionlink >= 1 ) { ?> call-for-action<?php } ?> clearer">
 	  <?php if($this->countModules('user3')) { ?>
-	  	<div id="nav">
+	  	<div id="nav" <?php if ( $topfish >= 1 ) { ?>class="topfish"<?php } if ( $topdrop >= 1 ) { ?>class="topdrop"<?php } ?>>
 	     <jdoc:include type="modules" name="user3" style="none" />
 	    </div>
 	  <?php } if($this->countModules('user4')) { ?>
