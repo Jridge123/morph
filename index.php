a8e95376085e9ed8e0bb618a80bd97a6e51e61a4
Chris Rault <Chris@chris.local>
2009-06-18 14:51:23 +0200

    fixed the error reporting option.

--- index.php
+++ index.php
@@ -6,10 +6,11 @@
 *** @copyright (C) 2009 by JoomlaJunkie
 *** @license   Commercial
 **/
-$error_reports = $this->params->get('error_reports');
-if ($error_reports =="1") { error_reporting(E_ALL ^ E_NOTICE); }
 defined( '_JEXEC' ) or die( 'Restricted index access' );
 include_once(JPATH_ROOT . "/templates/" . $this->template . '/core/morphFunctions.php');
+if ($error_reporting == 0) { 
+error_reporting(E_ALL ^ E_NOTICE); 
+}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>">
