dab8fb7e57ab7f5029b0ef7ccfef5a7b2dd47916
Chris Rault <Chris@chris.local>
2009-07-02 14:55:18 +0200

    tweaks

--- js/dynamic.js.php
+++ js/dynamic.js.php
@@ -55,14 +55,12 @@ header($expire);
 		});
 		$(".article-body p:first").addClass("teaser");
 		$("#nav li:first").addClass("first");
-
 		$(".sidenav li:first-child").each(function(){
 			$(this).addClass("first");
 		});
 		$(".sidenav li:last-child").each(function(){
 			$(this).addClass("last");
 		});
-
 		$("#secondary-content .module:first").addClass("firstmodule");
 		$("#secondary-content .module:last").addClass("lastmodule");
 		$("#tertiary-content .module:first").addClass("firstmodule");
@@ -70,28 +68,20 @@ header($expire);
 		$('img[align*=right]').addClass("img-right");
 		$('img[align*=left]').addClass("img-left");
 		$("table tr:even").addClass("alt");
-		
 		$("#user1 .modinner").wrapInner("<div class='extra-border'></div>");
 		$(".ui-tabs-panel").wrapInner("<div class='extra-box-border'></div>");		$(".ui-tabs-nav li").wrapInner("<span class='extra-tab-border'></span>");		$("input#mod_search_searchword").wrapInner("<div class='extra-search-border'></div>");
-		
 <?php if ( $topshelf_equalize == 1 ) { ?>
 		$(function(){ $('#topshelf .mod-grid').equalHeights(); });
-		
 <?php } if ( $bottomshelf_equalize == 1 ) { ?>
 		$(function(){ $('#bottomshelf .mod-grid').equalHeights(); });
-		
 <?php } if ( $user1count > 1 && $user1_equalize == 1 ) { ?>
 		$(function(){ $('#user1 .modinner').equalHeights(); });
-		
 <?php } if ( $user2count > 1 && $user2_equalize == 1 ) { ?>
 		$(function(){ $('#user2 .mod').equalHeights(); });
-		
 <?php } if ( $topshelfcount > 1 && $topshelf_equalize == 1 ) { ?>
 		$(function(){ $('#topshelf .modinner').equalHeights(); });
-		
 <?php } if ( $btmshelfcount > 1 && $bottomshelf_equalize == 1 ) { ?>
 		$(function(){ $('#topshelf .mod').equalHeights(); });
-		
 <?php } if ( $rounded_corners == 1 or $roundedcount !== '0' ) { ?>
 		$('.rounded h3').corners("10px top");
 		$('blockquote.rounded').corners("<?php echo $rounded_amount; ?>");
@@ -110,9 +100,7 @@ header($expire);
 		$('.right-pullquote').corners("10px");
 		$('.quote-author').corners("10px bottom");
 		$('.readon').corners("5px");
-
 <?php } if ( $topfish >= 1  ) { ?>
-
 		$("#nav .menu")<?php if ($topnav_supersubs == 1 ) { ?>.supersubs({
 		 minWidth: <?php echo $topnav_minwidth; ?>,
 		 maxWidth: <?php echo $topnav_maxwidth; ?>,
@@ -126,14 +114,13 @@ header($expire);
 		 hoverClass: 'sfHover',
 		 <?php if ($topnav_hoverintent == 0 ) { ?>disableHI: true<?php } else { ?>disableHI: false<?php } ?>
 		 })
-
+<?php } if ( $topdrop >= 1  ) { ?>	 		$("#nav .menu").superfish({			pathClass:  'current'		});
 <?php } if ( $animate_left == 1 ) { ?>	
 		$('#secondary-content ul.menu.slide li:not(.active) a, #tertiary-content ul.menu.slide li:not(.active) a').hoverIntent(function() { //mouse in  
 		    $(this).animate({backgroundColor: "#fff"}, 100).animate({ paddingLeft: '18px' }, "normal");
 			}, function() { // mouse out  
 		     $(this).animate({ paddingLeft: '10px' }, "normal").animate({ backgroundColor: "#000" }, "slow");  
 		  });
-		  
 <?php } if ( $sidefish >= 1 ) { ?> 
 	        $(".mod.sidefish ul.menu").superfish({ 
 	            animation: {height:'show'},   // slide-down effect without fade-in 
@@ -141,13 +128,10 @@ header($expire);
 	        });    
 <?php } if ( $toolbar_slider == 1 ) { ?>
    		initSlider('#toolbar', '<?php echo $toolbar_slider_text; ?>'); 
-   		
 <?php } if ( $topshelf_slider == 1 ) { ?>
    		initSlider('#topshelf', '<?php echo $topshelf_slider_text; ?>');
-   		
 <?php } if ( $bottomshelf_slider == 1 ) { ?>
 	    initSlider('#bottomshelf', '<?php echo $bottomshelf_slider_text; ?>');	
-	    
 <?php } if ( $topnav_hoverfocus == 1 ) { ?>
 		$('#nav ul.menu ul').hover(
 			function(){
@@ -157,19 +141,15 @@ header($expire);
 				$('#user1').fadeTo("fast", "1");
 			}
 		);
-		
 <?php } if ( $image_captions == 1 ) { ?>
-				$('img.captify').captify({});
-				
+		$('img.captify').captify({});
 <?php } if ( $toolbar_slider or $topshelf_slider or $bottomshelf_slider ) {
 		include('jquery.slider.js'); ?>
-		
 <?php } if ( $tabscount > 0 ) { 
 		for($n=1; $n <= $tabscount; $n++){
 		echo "\n\t\t$('#tabs$n').tabs({fx: {opacity: 'toggle', duration: 1}, cookie: {expires: 7, path: '/'}});";
 		}
 } ?>
-
 		$("#footer-links .fl-left li:last").addClass("fl-last");
 		$("#footer-links .fl-right li:last").addClass("fl-last");	
     })
