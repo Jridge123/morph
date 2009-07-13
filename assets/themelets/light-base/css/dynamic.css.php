30b820cb54a24062feea28402170329652353fa1
Chris Rault <Chris@chris.local>
2009-07-13 10:29:01 +0200

    tweaks

--- assets/themelets/light-base/css/dynamic.css.php
+++ assets/themelets/light-base/css/dynamic.css.php
@@ -48,9 +48,8 @@ function isIE6(){
 ?>
 body{<?php if ( $bg_color && $bg_color !== "#default" ) { ?>background-color:<?php echo $bg_color; ?>;<?php } ?>background-image:url(../../../backgrounds/<?php echo $bg_image; ?>);background-repeat:<?php echo $bg_repeat; ?>;background-position:<?php echo $bg_position; ?>;background-attachment:<?php echo $bg_attachment; ?>;}
 <?php if ( $logo_type == 2 ) { ?>
-#branding h1,#branding h1 a{width:<?php echo $logo_width; ?>px;height:<?php echo $logo_height; ?>px;}
+#branding h1,#branding h1 a{margin:0;padding:0;width:<?php echo $logo_width; ?>px;height:<?php echo $logo_height; ?>px;}
 #branding h1 a{background:transparent url(<?php echo $logo; ?>) no-repeat 0;}
-
 <?php } if ( $logo_type == 1 ) { ?>
 #branding.txt-logo a{<?php if ( $logo_fontfamily !== "" ) { ?>font-family:<?php echo $logo_fontfamily; ?>;<?php } if ( $logo_fontsize !== "" ) { ?>font-size:<?php echo $logo_fontsize; ?>;<?php } if ( $logo_textcolor && $logo_textcolor !== "#default" ) { ?>color:<?php echo $logo_textcolor; ?>;<?php } ?>}
 <?php } if ( $display_slogan == 1 ) { ?>
