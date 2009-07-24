f1d54c4c22fb93e9fdae628ae728ff700bf0d0ad
Chris Rault <Chris@chris.local>
2009-07-24 20:37:12 +0200

    fixes.

--- css/template.css.php
+++ css/template.css.php
@@ -69,20 +69,49 @@ if( $pack_css == 1 ){
 	include(JPATH . 'morph_assets/themelets/'.$themelet.'/css/modfx.css');
 }
 ?>
+
+		
+		
 <!-- dynamic css starts here: -->
 html body{<?php if ( $bg_color && $bg_color !== "#default" ) { ?>background-color:<?php echo $bg_color; ?>;<?php }; if ( $bg_image && $bg_image !== "Use themelets background" ) { ?>background-image:url(<?php $_SERVER['DOCUMENT_ROOT']; ?>/morph_assets/backgrounds/<?php echo $bg_image; ?>);<?php } ?>background-repeat:<?php echo $bg_repeat; ?>;background-position:<?php echo $bg_position; ?>;background-attachment:<?php echo $bg_attachment; ?>;}
-<?php if ( $logo_type == 2 ) { ?>
+
+
+
+
+<?php if ( $logo_type == 0 ) { ?>
+#branding h1,#branding h1 a{margin:0;padding:0;}
+#branding h1 a{
+	font-family:<?php echo $logo_fontfamily; ?>;
+	font-size:<?php echo $logo_fontsize; ?>;
+	<?php if ( $logo_textcolor !== "#default" ) { ?>color:<?php echo $logo_textcolor; ?>;<?php } ?>
+	padding:.7em 0 0 0;
+	display:block;
+}
+<?php } ?>
+
+
+
+
+<?php if ( $logo_type == 1 ) { ?>
 #branding h1,#branding h1 a{margin:0;padding:0;width:<?php echo $logo_width; ?>px;height:<?php echo $logo_height; ?>px;}
 #branding h1 a{background:transparent url(<?php echo $logo; ?>) no-repeat 0;}
-<?php } if ( $logo_type == 1 ) { ?>
+
+
+
+
+<?php } if ( $logo_type == 2 ) { ?>
 #branding.txt-logo a{
 <?php if ( $logo_fontfamily !== "" ) { ?>
-font-family:<?php echo $logo_fontfamily; ?>;
+	font-family:<?php echo $logo_fontfamily; ?>;
 <?php } if ( $logo_fontsize !== "" ) { ?>
-font-size:<?php echo $logo_fontsize; ?>;
+	font-size:<?php echo $logo_fontsize; ?>;
 <?php } if ( $logo_textcolor && $logo_textcolor !== "#default" ) { ?>
-color:<?php echo $logo_textcolor; ?>;<?php } ?>}
-<?php } if ( $display_slogan == 1 ) { ?>
+	color:<?php echo $logo_textcolor; ?>;<?php } ?>
+}
+<?php } ?>
+
+
+<?php if ( $display_slogan == 1 ) { ?>
 #branding.slogan .slogan{<?php if ( $slogan_fontfamily !== "" ) { ?>font-family:<?php echo $slogan_fontfamily; ?>;<?php } if ( $slogan_fontsize !== "" ) { ?>font-size:<?php echo $slogan_fontsize; ?>;<?php } if ( $slogan_textcolor && $slogan_textcolor !== "#default" ) { ?>color:<?php echo $slogan_textcolor; ?>;<?php } ?>}
 <?php } if ( $toolbar_slider == 1 ) { ?>
 #toolbar-wrap{position:absolute;top:0;width:100%;}
