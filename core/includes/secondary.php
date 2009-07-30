7580f49b7b48eb15dede9f8cfdf5414346343dd7
Chris Rault <Chris@chris.local>
2009-07-30 17:54:29 +0200

    split menu fix

--- core/includes/secondary.php
+++ core/includes/secondary.php
@@ -1,7 +1,7 @@
 <?php if ($this->countModules( 'splitleft or topleft or left or bottomleft' )) { ?>
 <div class="sidenav yui-b" id="secondary-content">
  <div class="inner">
-    <?php if ($this->countMenuChildren()){ ?>
+    <?php if ($this->countMenuChildren() || $this->countModules( 'splitleft' ) > 0 ) { ?>
     <jdoc:include type="modules" name="splitleft" style="<?php if( $debug_modules == 1 ){ echo 'outline'; } else { echo $splitleft_chrome; } ?>" />
     <?php } ?>
     <jdoc:include type="modules" name="topleft" style="<?php if( $debug_modules == 1 ){ echo 'outline'; } else { echo $topleft_chrome; } ?>" />
--- core/includes/tertiary.php
+++ core/includes/tertiary.php
@@ -1,6 +1,6 @@
 <?php if($this->countModules('right')) { ?>
 <div class="sidenav yui-u" id="tertiary-content">
-	<?php if ($this->countMenuChildren()){ ?>
+    <?php if ($this->countMenuChildren() || $this->countModules( 'splitright' ) > 0 ) { ?>
 	<jdoc:include type="modules" name="splitright" style="<?php if( $debug_modules == 1 ){ echo 'outline'; } else { echo $splitleft_chrome; } ?>" />
 	<?php } ?>
 	<jdoc:include type="modules" name="topright" style="<?php if( $debug_modules == 1 ){ echo 'outline'; } else { echo $topright_chrome; } ?>" />
