51190771c3a6c2357629af269a1e448c4428d53c
Byron Rode <Byron@Byrons-Mactizzy.local>
2009-07-30 09:31:47 +0200

    Module Headings :)

--- html/modules.php
+++ html/modules.php
@@ -28,6 +28,59 @@ jimport('joomla.application.module.helper');
  * NOTICE: All chrome wrapping methods should be named: modChrome_{STYLE} and take the same
  * two arguments.
  */
+ 
+ function moduleHeadings($modtitle){
+	// splitters
+	$pretext = '<';
+	$subtext = '>';
+	$twotone = '|';
+	
+	$spaces = array('<span class="mod-tone1"> ', '<span class="mod-tone2"> ', '<span class="mod-pretext"> ', '<span class="mod-subtext"> ', ' </span>');
+	$nospaces = array('<span class="mod-tone1">', '<span class="mod-tone2">', '<span class="mod-pretext">', '<span class="mod-subtext">', '</span>');
+	
+	// subtext & twotone
+	if(strstr($modtitle, $subtext) && strstr($modtitle, $twotone)){
+		$twotone_arr = explode($twotone, $modtitle);
+		$subtext_arr = explode($subtext, $twotone_arr[1]);
+		$str_twotone = '<span class="mod-tone1">'.$twotone_arr[0].'</span> <span class="mod-tone2">'.$subtext_arr[0].'</span>';
+		$str_subtext = '<span class="mod-subtext">'.$subtext_arr[1].'</span>';
+		
+		$string = $str_twotone .' '. $str_subtext;
+		return str_replace($spaces, $nospaces, $string);
+	}
+	
+	// subtext & twotone
+	if(strstr($modtitle, $pretext) && strstr($modtitle, $twotone)){
+		$pretext_arr = explode($pretext, $modtitle);
+		$twotone_arr = explode($twotone, $pretext_arr[1]);
+		$str_pretext = '<span class="mod-pretext">'.$pretext_arr[0].'</span>';
+		$str_twotone = '<span class="mod-tone1">'.$twotone_arr[0].'</span> <span class="mod-tone2">'.$twotone_arr[1].'</span>';
+		
+		$string = $str_pretext .' '. $str_twotone;
+		return str_replace($spaces, $nospaces, $string);
+	}
+	
+	if(strstr($modtitle, $twotone) && !strstr($modtitle, $pretext)){
+		$twotone_arr = explode($twotone, $modtitle);
+		$str_twotone = '<span class="mod-tone1">'.$twotone_arr[0].'</span> <span class="mod-tone2">'.$twotone_arr[1].'</span>';
+		
+		return str_replace($spaces, $nospaces, $str_twotone);
+	}
+	
+	if(strstr($modtitle, $pretext) && !strstr($modtitle, $twotone)){
+		$pretext_arr = explode($pretext, $modtitle);
+		$str_pretext = '<span class="mod-pretext">'.$pretext_arr[0].'</span>'.$pretext_arr[1];
+		
+		return str_replace($spaces, $nospaces, $str_pretext);
+	}
+	
+	if(strstr($modtitle, $subtext) && !strstr($modtitle, $twotone)){
+		$subtext_arr = explode($subtext, $modtitle);
+		$str_subtext = $subtext_arr[0].'<span class="mod-subtext">'.$subtext_arr[1].'</span>';
+		
+		return str_replace($spaces, $nospaces, $str_subtext);
+	}
+}
 
 /* Module chrome that allows for rounded corners by wrapping in nested div tags and adds extra hooks for ModFX styles */
 function modChrome_custom1($module, &$params, &$attribs) {
@@ -40,7 +93,7 @@ if ($pub_modules[0]->id == $module->id) {
 	$posSuffix = $params->get('moduleclass_sfx');
 } ?>
 <div class="mod mod-basic <?php echo $params->get('moduleclass_sfx'); ?>" id="mod<?php echo $module->id; ?>">
-	<?php if ($module->showtitle != 0) : ?><h3><span class="icon"></span><?php echo $module->title; ?></h3><?php endif; ?>
+	<?php if ($module->showtitle != 0) : ?><h3><span class="icon"></span><?php echo moduleHeadings($module->title); ?></h3><?php endif; ?>
 	<div class="modinner"><?php echo $module->content; ?></div>
 </div>
 <?php }
@@ -56,7 +109,7 @@ if ($pub_modules[0]->id == $module->id) {
 } 
 ?>
 <div class="mod mod-grid yui-u <?php echo $posSuffix; ?>" id="mod<?php echo $module->id; ?>">
-	<?php if ($module->showtitle != 0) : ?><h3><span class="icon"></span><?php echo $module->title; ?></h3><?php endif; ?>
+	<?php if ($module->showtitle != 0) : ?><h3><span class="icon"></span><?php echo moduleHeadings($module->title); ?></h3><?php endif; ?>
 	<?php echo $module->content; ?>
 </div>
 <?php }
@@ -72,7 +125,7 @@ if ($pub_modules[0]->id == $module->id) {
 } ?>
 	<div class="mod mod-fx yui-u <?php echo $posSuffix; ?>" id="mod<?php echo $module->id; ?>">
 		<div class="modinner">
-			<?php if ($module->showtitle != 0) : ?><h3><span class="icon"></span><?php echo $module->title; ?></h3><?php endif; ?>
+			<?php if ($module->showtitle != 0) : ?><h3><span class="icon"></span><?php echo moduleHeadings($module->title); ?></h3><?php endif; ?>
 			<?php echo $module->content; ?>
 		</div>
 	</div>
@@ -107,9 +160,9 @@ global $morph_tabs,$tabscount,$loadtabs,$istabsload;
 			$tabs_contents = '';
 			foreach ( $morph_tabs[$attribs['name']] as $modul ){
 				if ($curr_tab == 1) { ?>
-					<li class="ui-state-default ui-tabs-selected"><a href="#tab<?php echo $curr_tab.'-'.$modul->position; ?>"><?php echo $modul->title;?></a></li>
+					<li class="ui-state-default ui-tabs-selected"><a href="#tab<?php echo $curr_tab.'-'.$modul->position; ?>"><?php echo moduleHeadings($modul->title);?></a></li>
 				<?php } else { ?>
-					<li class="ui-state-default"><a href="#tab<?php echo $curr_tab.'-'.$modul->position; ?>"><?php echo $modul->title;?></a></li>
+					<li class="ui-state-default"><a href="#tab<?php echo $curr_tab.'-'.$modul->position; ?>"><?php echo moduleHeadings($modul->title);?></a></li>
 				<?php 
 				}
 				$tabs_contents .= '<div id="tab'.$curr_tab.'-'.$modul->position.'" class="ui-tabs-panel">'.$modul->content.'</div>';
