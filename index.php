63944fa8ffb07b563f02c83af3a39200b3f89052
Chris Rault <Chris@chris.local>
2009-08-06 15:43:34 +0200

    Basic implementation of iphone support.

--- index.php
+++ index.php
@@ -18,38 +18,44 @@ error_reporting(E_ALL ^ E_NOTICE);
 <jdoc:include type="head" />
 </head>
 <body class="js-disabled <?php echo "$lcbrowser $lcbrowser$ver"; if ($pageclass != ""){ echo ' '.$pageclass; } ?>"<?php if ($themelet != ""){ echo ' id="'.$themelet.'"'; } ?>>
-<?php if ( isIE6() && $ie6_upgrade == 1 ) include_once("core/includes/ie6upgrade.php"); ?>
-<?php if($this->countModules('advert1')) { ?><div id="advert1"><jdoc:include type="modules" name="advert1" style="none" /></div><?php } ?>
-<?php
-// toolbar + topnav after toolbar
-if ( $toolbar_position == 0 ) include_once("core/includes/toolbar.php");
-if ( $topnav_position == 0 ) include_once("core/includes/topnav.php");
+<?php $browser = new Browser();
+if( $browser->getBrowser() == Browser::PLATFORM_IPHONE ) {
+	include_once("core/includes/iphone.php");
+} else { ?>
+	<?php if ( isIE6() && $ie6_upgrade == 1 ) include_once("core/includes/ie6upgrade.php"); ?>
+	<?php if($this->countModules('advert1')) { ?><div id="advert1"><jdoc:include type="modules" name="advert1" style="none" /></div><?php } ?>
+	<?php
+	// toolbar + topnav after toolbar
+	if ( $toolbar_position == 0 ) include_once("core/includes/toolbar.php");
+	if ( $topnav_position == 0 ) include_once("core/includes/topnav.php");
+	
+	include_once("core/includes/masthead.php");
+	
+	// toolbar + topnav after master head
+	if ( $toolbar_position == 1 ) include_once("core/includes/toolbar.php");
+	if ( $topnav_position == 1 ) include_once("core/includes/topnav.php");
+	
+	include_once("core/includes/subhead.php");
+	
+	// toolbar + topnav after sub head
+	if ( $toolbar_position == 2 ) include_once("core/includes/toolbar.php");
+	if ( $topnav_position == 2 ) include_once("core/includes/topnav.php");
+	
+	include_once("core/includes/topshelf.php");
+	
+	// toolbar + topnav after topshelf
+	if ( $toolbar_position == 3 ) include_once("core/includes/toolbar.php");
+	if ( $topnav_position == 3 ) include_once("core/includes/topnav.php");
+	
+	include_once("core/includes/main.php");
+	include_once("core/includes/bottomshelf.php");
+	include_once("core/includes/foot.php");
+	?>
+	<?php if($this->countModules('advert2')) { ?><div id="advert2"><jdoc:include type="modules" name="advert2" style="none" /></div><?php } ?>
+	<jdoc:include type="modules" name="debug" />
+	<?php include_once("core/includes/ga-code.php"); ?>
+	<?php if ( $plugin_scrollto == 1 ) { ?><a href="#top" id="top-link">Top of Page</a><?php } ?>
 
-include_once("core/includes/masthead.php");
-
-// toolbar + topnav after master head
-if ( $toolbar_position == 1 ) include_once("core/includes/toolbar.php");
-if ( $topnav_position == 1 ) include_once("core/includes/topnav.php");
-
-include_once("core/includes/subhead.php");
-
-// toolbar + topnav after sub head
-if ( $toolbar_position == 2 ) include_once("core/includes/toolbar.php");
-if ( $topnav_position == 2 ) include_once("core/includes/topnav.php");
-
-include_once("core/includes/topshelf.php");
-
-// toolbar + topnav after topshelf
-if ( $toolbar_position == 3 ) include_once("core/includes/toolbar.php");
-if ( $topnav_position == 3 ) include_once("core/includes/topnav.php");
-
-include_once("core/includes/main.php");
-include_once("core/includes/bottomshelf.php");
-include_once("core/includes/foot.php");
-?>
-<?php if($this->countModules('advert2')) { ?><div id="advert2"><jdoc:include type="modules" name="advert2" style="none" /></div><?php } ?>
-<jdoc:include type="modules" name="debug" />
-<?php include_once("core/includes/ga-code.php"); ?>
-<?php if ( $plugin_scrollto == 1 ) { ?><a href="#top" id="top-link">Top of Page</a><?php } ?>
+<?php } ?>
 </body>
 </html>
