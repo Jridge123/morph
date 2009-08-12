f1779244cbf4dd8dec325ed0100a479188e6d03a
Chris Rault <Chris@chris.local>
2009-08-12 11:49:27 +0200

    updated with new function.

--- core/includes/user1.php
+++ core/includes/user1.php
@@ -1,7 +1,5 @@
-<?php if($this->countModules('user1') && $user1_show == 0 ) { ?>
-<div id="user1" class="intelli <?php getYuiSuffix('user1', $jj_const); ?> <?php echo $user1_chrome ?> modcount<?php echo $user1count ?>">
-	<?php if ( $user1_inner == "1" ) { ?><div id="user1-inner"><?php } ?>
-	<jdoc:include type="modules" name="user1" style="<?php if( $debug_modules == 1 ){ echo 'outline'; } else { echo $user1_chrome; } ?>" />
-	<?php if ( $user1_inner == "1" ) { ?></div><?php } ?>
-</div>
-<?php } ?>
+<?php 
+$position = 'user1';
+include dirname(__FILE__) . '/../morphBlockClasses.php';
+echo blocks($position, $this, $jj_const, $classes, $site_width, $debug_modules); 
+?>
--- core/includes/user2.php
+++ core/includes/user2.php
@@ -1,7 +1,5 @@
-<?php if($this->countModules('user2') && $user2_show == 0 ) { ?>
-<div id="user2" class="intelli <?php getYuiSuffix('user2', $jj_const); ?> <?php echo $user2_chrome ?> modcount<?php echo $user2count ?>">
-	<?php if ( $user2_inner == 1 ) { ?><div id="user2-inner"><?php } ?>
-	<jdoc:include type="modules" name="user2" style="<?php if( $debug_modules == 1 ){ echo 'outline'; } else { echo $user2_chrome; } ?>" />
-	<?php if ( $user2_inner == 1 ) { ?></div><?php } ?>
-</div>
-<?php } ?>
+<?php 
+$position = 'user2';
+include dirname(__FILE__) . '/../morphBlockClasses.php';
+echo blocks($position, $this, $jj_const, $classes, $site_width, $debug_modules); 
+?>
