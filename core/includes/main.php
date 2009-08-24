26699fdb044ed1cd9a647aca3f2024c34b77eb8b
Chris Rault <Chris@chris.local>
2009-08-24 14:45:22 +0200

    fixed the main block inner wrapping.

--- core/includes/main.php
+++ core/includes/main.php
@@ -1,7 +1,7 @@
 <?php if ( $main_wrap == "1" ) { ?><div id="bd-wrap"><?php } ?>
 	<!-- apply outer scheme -->
 	<div id="bd" class="yui-t<?php echo $CurrentOuterScheme ?> <?php echo $site_width ?><?php if($option=='com_content' && $layout=='form'){ ?> editmode<?php } ?>">
-	<div class="bd-inner">
+	<?php if ( $main_inner == 1 ) { ?><div class="bd-inner"><?php } ?>
 	<?php if($this->countModules('user1')) { ?><?php include_once("user1.php") ?><?php } ?>
 		<div id="yui-main">
 			<div class="yui-b<?php if (!$this->countModules('splitleft or topleft or left or bottomleft')) { echo ' no-left'; } ?>">
@@ -12,7 +12,7 @@
 					<div class="primary-inner">
 						<?php $position = 'inset2'; include dirname(__FILE__) . '/../morphBlockClasses.php'; echo blocks($position, $this, $jj_const, $classes, '', $debug_modules, $nojs); ?>
 						<?php if ((JRequest::getVar( 'view' ) != 'frontpage') && ($this->countModules('breadcrumb'))) { ?>
-							<div id="breadcrumbs"><span>You are here: </span><jdoc:include type="modules" name="breadcrumb" /></div>
+							<div id="breadcrumbs"><?php if($pathway_text !== '') { ?><span><?php echo $pathway_text ?> </span><?php } ?><jdoc:include type="modules" name="breadcrumb" /></div>
 						<?php } ?>
 						<jdoc:include type="message" />
 						<jdoc:include type="component" />
@@ -24,8 +24,8 @@
 			</div>
 		</div>
 	</div>
-	<?php if($CurrentOuterScheme !== "10") { include_once("secondary.php"); ?><?php } ?>
-	<?php if($this->countModules('user2')) { ?><?php include_once("user2.php") ?><?php } ?>
-	</div>
+	<?php if($CurrentOuterScheme !== "10") { include_once("secondary.php"); } ?>
+	<?php if($this->countModules('user2')) { include_once("user2.php"); } ?>
+	<?php if ( $main_inner == 1 ) { '</div>'; } ?>
 </div>
 <?php if ( $main_wrap == "1" ) { ?></div><?php } ?>
