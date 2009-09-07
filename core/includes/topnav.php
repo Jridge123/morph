28e6dbc8bfc44b1d68aa93e597f0e11ae174b2b1
Andy Neale <Andy@Andys-MacBook-Pro-5.local>
2009-09-07 16:41:52 +0200

    mod to subtext class

--- core/includes/topnav.php
+++ core/includes/topnav.php
@@ -1,5 +1,5 @@
 <?php if ($this->countModules( 'user3 or user4' ) && $topnav_show == 0 ) { ?>
-<?php if ( $topnav_wrap == 1 ) { ?><div id="topnav-wrap"<?php if ( $topfish >= 1 ) { ?> class="topfish <?php if ( $subtext_top >= 1 ) { ?> subtext<?php } ?>"<?php } if ( $topdrop >= 1 ) { ?> class="topdrop"<?php } ?>><?php } ?>
+<?php if ( $topnav_wrap == 1 ) { ?><div id="topnav-wrap"<?php if ( $topfish >= 1 ) { ?> class="topfish <?php if ( $subtext_top >= 1 ) { ?> subtext<?php } ?>"<?php } if ( $topdrop >= 1 ) { ?> class="topdrop <?php if ( $subtext_top >= 1 ) { ?> subtext<?php } ?>"<?php } ?>><?php } ?>
 	<div id="topnav" class="<?php echo $site_width; if ( $subtext_top >= 1 ) { ?> subtext<?php } if ( $topnav_actionlink >= 1 ) { ?> call-for-action<?php } ?> clearer<?php if ( $topfish >= 1 ) { ?> topfish<?php } if ( $topdrop >= 1 ) { ?> topdrop<?php } ?>">
 	  <?php if($this->countModules('user3')) { ?>
 	  	<div id="nav" class="clearer">
