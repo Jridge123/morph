420489fb0ff893909c9dd1b194abae46ef87d820
Byron Rode <Byron@Byrons-Mactizzy.local>
2009-09-14 12:08:07 +0200

    Set component Defaults to read from DB.

--- core/InnerLayout.php
+++ core/InnerLayout.php
@@ -1,38 +1,14 @@
 <?php
+$innerScheme = array ('default'=>"$inner_default");
+if(!preg_match('/administrator/i', $_SERVER['REQUEST_URI'])){
+	foreach($MORPH as $k => $v){
+ 		if(preg_match('/id_/i', $k)){
+ 			$k = str_replace('id_', '', $k);
+ 			$innerScheme[$k] = $v;
+ 		}
+ 	}
+};
 
-// This is used to configure your inner layout defaults per component. 
-// YUI CSS uses percentages to determine the ratio between the Inner Layout
-// and the Primary Content. The first value is the width of the content and 
-// the second is the Inner Layout (right sidebar).
-
-/* Options Explained:
-**************************************************/
-// g   = 50/50 Split.
-// gc  = 66/33 Split.
-// gd  = 33/66 Split - Not Recommended.
-// ge  = 75/25 Split.
-// gf  = 25/75 Split - Not Recommended.
-
-/* Global default - Set within configurator:
-**************************************************/
-	$innerScheme = array (
-	'default'=>     	 	"$inner_default",
-
-/* Example Usage:
-**************************************************/
-//	'com_frontpage'=>		'g',
-//	'com_wrapper'=>			'ge',
-//	'com_fireboard'=>		'gd',
-//	'com_content'=>			'ge',
-//	'com_newsfeeds'=>		'gd',
-//	'com_weblinks'=>		'gh',
-//	'com_user'=>			'gf',
-//	'com_contact'=>			'ge',
-//	'com_search'=>			'gh',
-);
-
-/* Do not modify anything below this line:
-**************************************************/
 if ($option && isset($innerScheme[$option]) && trim($innerScheme[$option])!= false){
 	$CurrentInnerScheme = trim($innerScheme[$option]);
 } 
