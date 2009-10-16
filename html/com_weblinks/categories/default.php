ef2f4c8d655e259b2de58003c212099244b7f986
Chris Rault <Chris@chris.local>
2009-10-16 12:42:33 +0200

    Tweaked the weblinks html overrides, still had some table code.

--- html/com_weblinks/categories/default.php
+++ html/com_weblinks/categories/default.php
@@ -7,18 +7,14 @@ defined('_JEXEC') or die('Restricted access'); ?>
 <?php endif; ?>
 
 <?php if ( ($this->params->def('image', -1) != -1) || $this->params->def('show_comp_description', 1) ) : ?>
-<table width="100%" cellpadding="4" cellspacing="0" border="0" align="center" class="contentpane<?php echo $this->escape($this->params->get('pageclass_sfx')); ?>">
-<tr>
-	<td valign="top" class="contentdescription<?php echo $this->escape($this->params->get('pageclass_sfx')); ?>">
+<div class="weblinks-desc teaser">
 	<?php
 		if ( isset($this->image) ) :  echo $this->image; endif;
 		echo $this->params->get('comp_description');
 	?>
-	</td>
-</tr>
-</table>
+</div>
 <?php endif; ?>
-<ul>
+<ul id="weblinks">
 <?php foreach ( $this->categories as $category ) : ?>
 	<li>
 		<a href="<?php echo $category->link; ?>" class="category<?php echo $this->escape($this->params->get( 'pageclass_sfx' )); ?>">
