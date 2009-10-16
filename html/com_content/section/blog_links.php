2652fcae8577f72a9c834863c531da94f92130f6
Chris Rault <Chris@chris.local>
2009-10-16 12:42:04 +0200

    Tweaked the section list html override.

--- html/com_content/section/blog_links.php
+++ html/com_content/section/blog_links.php
@@ -2,8 +2,8 @@
 defined('_JEXEC') or die('Restricted access');
 ?>
 <h3><?php echo JText::_('More Articles...'); ?></h3>
-<ul>
+<ul id="section-list">
 	<?php foreach ($this->links as $link) : ?>
-	<li><a class="blogsection" href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($link->slug, $link->catslug, $link->sectionid)); ?>"><?php echo $link->title; ?></a></li>
+	<li class="blogsection"><a href="<?php echo JRoute::_(ContentHelperRoute::getArticleRoute($link->slug, $link->catslug, $link->sectionid)); ?>"><?php echo $link->title; ?></a></li>
 	<?php endforeach; ?>
 </ul>
