99b179912a6329a9be48e61d7189a3e77d2c555e
Chris Rault <Chris@chris.local>
2009-11-07 11:59:48 +0200

    re-wrote the contact category layout (streamlined the code and added appropriate styling hooks).

--- html/com_contact/category/default.php
+++ html/com_contact/category/default.php
@@ -3,18 +3,15 @@
  * $Id: default.php 11917 2009-05-29 19:37:05Z ian $
  */
 defined( '_JEXEC' ) or die( 'Restricted access' );
-
 $cparams =& JComponentHelper::getParams('com_media');
-?>
-
-<?php if ( $this->params->get( 'show_page_title', 1 ) ) : ?>
-    <h1 class="componentheading clearer">
-        <?php echo $this->escape($this->params->get('page_title')); ?>
-    </h1>
+if ( $this->params->get( 'show_page_title', 1 ) ) : ?>
+<h1 class="componentheading clearer">
+    <?php echo $this->escape($this->params->get('page_title')); ?>
+</h1>
 <?php endif; ?>
-<div class="contentpane<?php echo $this->escape($this->params->get('pageclass_sfx')); ?>">
+<div id="contact-table">
 <?php if ($this->category->image || $this->category->description) : ?>
-	<div class="contentdescription<?php echo $this->escape($this->params->get('pageclass_sfx')); ?>">
+	<div class="category-desc">
 	<?php if ($this->params->get('image') != -1 && $this->params->get('image') != '') : ?>
 		<img src="<?php echo $this->baseurl .'/'. 'images/stories' . '/'. $this->params->get('image'); ?>" align="<?php echo $this->params->get('image_align'); ?>" hspace="6" alt="<?php echo JText::_( 'Contacts' ); ?>" />
 	<?php elseif ($this->category->image) : ?>
@@ -33,72 +30,65 @@ $cparams =& JComponentHelper::getParams('com_media');
 }
 </script>
 <form action="<?php echo $this->action; ?>" method="post" name="adminForm">
-<table width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
-	<thead>
-		<tr>
-			<td align="right" colspan="6">
-			<?php if ($this->params->get('show_limit')) :
-				echo JText::_('Display Num') .'&nbsp;';
-				echo $this->pagination->getLimitBox();
-			endif; ?>
-			</td>
-		</tr>
-	</thead>
-	<tfoot>
-		<tr>
-			<td align="center" colspan="6" class="sectiontablefooter<?php echo $this->escape($this->params->get('pageclass_sfx')); ?>">
-				<?php echo $this->pagination->getPagesLinks(); ?>
-			</td>
-		</tr>
-		<tr>
-			<td colspan="6" align="right">
-				<?php echo $this->pagination->getPagesCounter(); ?>
-			</td>
-		</tr>
-	</tfoot>
-	<tbody>
-	<?php if ($this->params->get( 'show_headings' )) : ?>
-		<tr>
-			<td width="5" align="right" class="sectiontableheader<?php echo $this->escape($this->params->get('pageclass_sfx')); ?>">
-				<?php echo JText::_('Num'); ?>
-			</td>
-			<td height="20" class="sectiontableheader<?php echo $this->escape($this->params->get('pageclass_sfx')); ?>">
-				<?php echo JHTML::_('grid.sort',  'Name', 'cd.name', $this->lists['order_Dir'], $this->lists['order'] ); ?>
-			</td>
-			<?php if ( $this->params->get( 'show_position' ) ) : ?>
-			<td height="20" class="sectiontableheader<?php echo $this->escape($this->params->get('pageclass_sfx')); ?>">
-				<?php echo JHTML::_('grid.sort',  'Position', 'cd.con_position', $this->lists['order_Dir'], $this->lists['order'] ); ?>
-			</td>
-			<?php endif; ?>
-			<?php if ( $this->params->get( 'show_email' ) ) : ?>
-			<td height="20" width="20%" class="sectiontableheader<?php echo $this->escape($this->params->get('pageclass_sfx')); ?>">
-				<?php echo JText::_( 'Email' ); ?>
-			</td>
-			<?php endif; ?>
-			<?php if ( $this->params->get( 'show_telephone' ) ) : ?>
-			<td height="20" width="15%" class="sectiontableheader<?php echo $this->escape($this->params->get('pageclass_sfx')); ?>">
-				<?php echo JText::_( 'Phone' ); ?>
-			</td>
-			<?php endif; ?>
-			<?php if ( $this->params->get( 'show_mobile' ) ) : ?>
-			<td height="20" width="15%" class="sectiontableheader<?php echo $this->escape($this->params->get('pageclass_sfx')); ?>">
-				<?php echo JText::_( 'Mobile' ); ?>
-			</td>
-			<?php endif; ?>
-			<?php if ( $this->params->get( 'show_fax' ) ) : ?>
-				<td height="20" width="15%" class="sectiontableheader<?php echo $this->escape($this->params->get('pageclass_sfx')); ?>">
-					<?php echo JText::_( 'Fax' ); ?>
-				</td>
-			<?php endif; ?>
-		</tr>
-	<?php endif; ?>
-	<?php echo $this->loadTemplate('items'); ?>
-</tbody>
-</table>
-
-<input type="hidden" name="option" value="com_contact" />
-<input type="hidden" name="catid" value="<?php echo $this->category->id;?>" />
-<input type="hidden" name="filter_order" value="<?php echo $this->lists['order']; ?>" />
-<input type="hidden" name="filter_order_Dir" value="" />
+    <?php if ($this->params->get('show_limit')) : ?>
+    <div class="table-limit">
+    	<?php echo JText::_('Display Num') .'&nbsp;';
+    	echo $this->pagination->getLimitBox(); ?>
+    </div>
+    <?php endif; ?>
+    <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
+    	<?php if ($this->params->get( 'show_headings' )) : ?>
+    	<thead>
+    		<tr>
+    			<th class="tbl-number">
+    			<?php echo JText::_('Num'); ?>
+    			</th>
+    			<th class="tbl-name">
+    				<?php echo JHTML::_('grid.sort',  'Name', 'cd.name', $this->lists['order_Dir'], $this->lists['order'] ); ?>
+    			</th>
+    			<?php if ( $this->params->get( 'show_position' ) ) : ?>
+    			<th class="tbl-position">
+    				<?php echo JHTML::_('grid.sort',  'Position', 'cd.con_position', $this->lists['order_Dir'], $this->lists['order'] ); ?>
+    			</th>
+    			<?php endif; if ( $this->params->get( 'show_email' ) ) : ?>
+    			<th class="tbl-email">
+    				<?php echo JText::_( 'Email' ); ?>
+    			</th>
+    			<?php endif; if ( $this->params->get( 'show_telephone' ) ) : ?>
+    			<th class="tbl-tel">
+    				<?php echo JText::_( 'Phone' ); ?>
+    			</th>
+    			<?php endif; if ( $this->params->get( 'show_mobile' ) ) : ?>
+    			<th class="tbl-mobile">
+    				<?php echo JText::_( 'Mobile' ); ?>
+    			</th>
+    			<?php endif; if ( $this->params->get( 'show_fax' ) ) : ?>
+    			<th class="tbl-fax">
+    				<?php echo JText::_( 'Fax' ); ?>
+    			</th>
+    			<?php endif; ?>
+    		</tr>
+    	</thead>
+    	<?php endif; ?>
+    	<tfoot>
+    		<tr>
+    			<td colspan="6">
+    				<?php echo $this->pagination->getPagesLinks(); ?>
+    			</td>
+    		</tr>
+    		<tr>
+    			<td colspan="6" align="right">
+    				<?php echo $this->pagination->getPagesCounter(); ?>
+    			</td>
+    		</tr>
+    	</tfoot>
+    	<tbody>
+    	    <?php echo $this->loadTemplate('items'); ?>
+        </tbody>
+    </table>
+    <input type="hidden" name="option" value="com_contact" />
+    <input type="hidden" name="catid" value="<?php echo $this->category->id;?>" />
+    <input type="hidden" name="filter_order" value="<?php echo $this->lists['order']; ?>" />
+    <input type="hidden" name="filter_order_Dir" value="" />
 </form>
 </div>
--- html/com_contact/category/default_items.php
+++ html/com_contact/category/default_items.php
@@ -3,36 +3,36 @@
 defined( '_JEXEC' ) or die( 'Restricted access' );
 ?>
 <?php foreach($this->items as $item) : ?>
-<tr class="sectiontableentry<?php echo $item->odd + 1; ?>">
-	<td align="right" width="5">
+<tr class="row<?php echo $item->odd + 1; ?>">
+	<td class="tbl-number">
 		<?php echo $item->count +1; ?>
 	</td>
-	<td height="20">
+	<td class="tbl-name">
 		<a href="<?php echo $item->link; ?>" class="category<?php echo $this->escape($this->params->get('pageclass_sfx')); ?>">
 			<?php echo $item->name; ?></a>
 	</td>
 	<?php if ( $this->params->get( 'show_position' ) ) : ?>
-	<td>
+	<td class="tbl-position">
 		<?php echo $this->escape($item->con_position); ?>
 	</td>
 	<?php endif; ?>
 	<?php if ( $this->params->get( 'show_email' ) ) : ?>
-	<td width="20%">
+	<td class="tbl-email">
 		<?php echo $item->email_to; ?>
 	</td>
 	<?php endif; ?>
 	<?php if ( $this->params->get( 'show_telephone' ) ) : ?>
-	<td width="15%">
+	<td class="tbl-tel">
 		<?php echo $this->escape($item->telephone); ?>
 	</td>
 	<?php endif; ?>
 	<?php if ( $this->params->get( 'show_mobile' ) ) : ?>
-	<td width="15%">
+	<td class="tbl-mobile">
 		<?php echo $this->escape($item->mobile); ?>
 	</td>
 	<?php endif; ?>
 	<?php if ( $this->params->get( 'show_fax' ) ) : ?>
-	<td width="15%">
+	<td class="tbl-fax">
 		<?php echo $this->escape($item->fax); ?>
 	</td>
 	<?php endif; ?>
