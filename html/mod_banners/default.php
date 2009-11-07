2f523019597bdbb262286e3a2888cb13ff0d28cd
Chris Rault <Chris@chris.local>
2009-11-07 12:00:44 +0200

    streamlined the banners module override.

--- html/mod_banners/default.php
+++ html/mod_banners/default.php
@@ -1,9 +1,9 @@
 <?php // no direct access
 defined('_JEXEC') or die('Restricted access'); ?>
-<div class="bannergroup <?php echo $params->get( 'moduleclass_sfx' ) ?>">
-<?php if ($headerText) : ?><h4 class="bannerheader"><?php echo $headerText ?></h4><?php endif; ?>
+<div class="bannergroup">
+<?php if ($headerText) : ?><h4><?php echo $headerText ?></h4><?php endif; ?>
 <ul>
-<?php foreach($list as $item) : ?><li class="banneritem"><?php echo modBannersHelper::renderBanner($params, $item); ?></li><?php endforeach; ?>
+<?php foreach($list as $item) : ?><li><?php echo modBannersHelper::renderBanner($params, $item); ?></li><?php endforeach; ?>
 </ul>
-<?php if ($footerText) : ?><p class="bannerfooter"><?php echo $footerText ?></p><?php endif; ?>
+<?php if ($footerText) : ?><p><?php echo $footerText ?></p><?php endif; ?>
 </div>
