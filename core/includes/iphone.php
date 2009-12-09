34c88fc3ed241157514593a4b81b29e1daf87a83
Chris Rault <Chris@chris.local>
2009-12-09 13:31:53 +0200

    tweaked the iphone logo area.

--- core/includes/iphone.php
+++ core/includes/iphone.php
@@ -1,7 +1,7 @@
 <div id="iphone-wrap">
 	<div id="iphone-header"<?php if($iphone_header){ ?> style="background-image:url(<?php echo $assetspath; ?>/iphone/<?php echo $iphone_header; ?>);"<?php } ?><?php if($iphone_webclip){ ?> class="webclip"<?php } ?>>
-	<a href="<?php echo JURI::root() ?>" title="<?php if ($logo_linktitle != ""){ echo $logo_linktitle; } else { echo $mainframe->getCfg('sitename'); } ?>">
 	    <?php if($iphone_webclip){ ?><img src="<?php echo $assetspath; ?>/iphone/<?php echo $iphone_webclip; ?>" width="30px" border="0" /><?php } ?>
+	    <a href="<?php echo JURI::root() ?>" title="<?php if ($logo_linktitle != ""){ echo $logo_linktitle; } else { echo $mainframe->getCfg('sitename'); } ?>">
 		<span><?php if ($logo_text != ""){ echo $logo_text; } else { echo $mainframe->getCfg('sitename'); } ?></span>
 	</a>
 	</div>
