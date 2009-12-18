eb5b1e85d3159375c3921cc415098111bbe30d85
connectr <chris@prothemer.com>
2009-12-18 14:58:00 +0200

    added closing php tag.

--- core/morphArrays.php
+++ core/morphArrays.php
@@ -8,3 +8,5 @@ $js_slider = array($toolbar_slider, $topshelf_slider, $bottomshelf_slider);
 $js_equalize = array($topshelf_equalize, $bottomshelf_equalize, $user1_equalize, $user2_equalize, $outer1_equalize, $outer2_equalize, $outer3_equalize, $outer4_equalize, $outer5_equalize, $inner1_equalize, $inner2_equalize, $inner3_equalize, $inner4_equalize, $inner5_equalize);
 
 // css arrays
+
+?>
