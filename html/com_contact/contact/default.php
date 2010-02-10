e3cee28de05fd0e0c07666f51e93a581ae65aeb5
connectr <chris@prothemer.com>
2010-02-11 00:01:46 +0200

    Added some conditional wraps to the contact layout html overrides, to ensure that there are no leftovers if any of the fields are turned off.

--- html/com_contact/contact/default.php
+++ html/com_contact/contact/default.php
@@ -29,17 +29,20 @@ $cparams = JComponentHelper::getParams ('com_media');
     </div>
     <?php endif; ?>
 
+	<?php if ($this->params->get('show_street_address') || $this->params->get('show_state') || $this->params->get('show_suburb') || $this->params->get('show_postcode') || $this->params->get('show_country') || $this->params->get('show_image')) : ?>
     <div id="contact-top">
+		<?php if ($this->params->get('show_street_address') || $this->params->get('show_state') || $this->params->get('show_suburb') || $this->params->get('show_postcode') || $this->params->get('show_country')) : ?>
 	    <div class="contact-info">
         	<?php echo $this->loadTemplate('address'); ?>
         </div>
-
+		<?php endif; ?>
 		<?php if ( $this->contact->image && $this->contact->params->get( 'show_image' ) ) : ?>
 		<div class="contact-photo">
 			<?php echo JHTML::_('image', 'images/stories' . '/'.$this->contact->image, JText::_( 'Contact' ), array('align' => 'middle', 'class' => 'photo')); ?>
 		</div>
 		<?php endif; ?>
     </div>           
+	<?php endif; ?>
 	
     <?php if ( $this->contact->params->get('show_email_form') && ($this->contact->email_to || $this->contact->user_id))
 	    echo $this->loadTemplate('form');
