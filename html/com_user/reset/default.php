e21a5f37463c5f2221dd3e0a36523891db77bd98
connectr <chris@prothemer.com>
2010-02-11 12:30:49 +0200

    tweaks to the remind and reset layouts to allow access via a lightbox.

--- html/com_user/reset/default.php
+++ html/com_user/reset/default.php
@@ -1,18 +1,17 @@
 <?php defined('_JEXEC') or die; ?>
-
-<div id="login-wrap">
-
-<?php if ( $this->params->def( 'show_page_title', 1 ) ) : ?>
-	<h1><?php echo $this->escape($this->params->get('page_title')); ?></h1>
-<?php endif; ?>
-<p class="login-description"><?php echo JText::_('RESET_PASSWORD_REQUEST_DESCRIPTION'); ?></p>
-
-<form action="<?php echo JRoute::_( 'index.php?option=com_user&task=requestreset' ); ?>" method="post" id="login-form" class="josForm form-validate forgot-pass">
-<ul>
-	<li class="label"><label for="email" class="hasTip" title="<?php echo JText::_('RESET_PASSWORD_EMAIL_TIP_TITLE'); ?>::<?php echo JText::_('RESET_PASSWORD_EMAIL_TIP_TEXT'); ?>"><?php echo JText::_('Email Address'); ?>:</label>
-		<span class="input-wrap"><input id="email" name="email" type="text" class="required validate-email form-input" /></span></li>
-	<li class="login-btn"><input type="submit" name="Submit" class="button validate" value="<?php echo JText::_('Submit') ?>" /></li>
-</ul>
-	<?php echo JHTML::_( 'form.token' ); ?>
-</form>
+<div id="user-reset">
+	<div id="login-wrap">
+	<?php if ( $this->params->def( 'show_page_title', 1 ) ) : ?>
+		<h1><?php echo $this->escape($this->params->get('page_title')); ?></h1>
+	<?php endif; ?>
+	<p class="login-description"><?php echo JText::_('RESET_PASSWORD_REQUEST_DESCRIPTION'); ?></p>
+	<form action="<?php echo JRoute::_( 'index.php?option=com_user&task=requestreset' ); ?>" method="post" id="login-form" class="josForm form-validate forgot-pass">
+	<ul>
+		<li class="label"><label for="email" class="hasTip" title="<?php echo JText::_('RESET_PASSWORD_EMAIL_TIP_TITLE'); ?>::<?php echo JText::_('RESET_PASSWORD_EMAIL_TIP_TEXT'); ?>"><?php echo JText::_('Email Address'); ?>:</label>
+			<span class="input-wrap"><input id="email" name="email" type="text" class="required validate-email form-input" /></span></li>
+		<li class="login-btn"><input type="submit" name="Submit" class="button validate" value="<?php echo JText::_('Submit') ?>" /></li>
+	</ul>
+		<?php echo JHTML::_( 'form.token' ); ?>
+	</form>
+	</div>
 </div>
