bdc39f2e4bf81bcd0358116a7c2a8be5f0bfb6fb
Andy Neale <Andy@Andys-MacBook-Pro-5.local>
2010-02-24 10:09:00 +0200

    added topnav inner div option and tweaked topdrop bar code

--- core/includes/topnav.php
+++ core/includes/topnav.php
@@ -3,8 +3,8 @@
 <?php if ( $topnav_wrap == 1 ) { ?>
 <div id="topnav-wrap" class="<?php echo pt_classes(array('subtext' => $subtext, 'topdrop' => $topdrop, 'topfish' => $topfish)); ?>">
 <?php } ?>
-	<div id="topnav" class="<?php echo pt_classes(array('subtext' => $subtext, 'topnav_actionlink' => $topnav_actionlink, 'topdrop' => $topdrop, 'topfish' => $topfish), $site_width); ?> primary-nav">
-
+	<div id="topnav" class="<?php echo pt_classes(array('subtext' => $subtext, 'topnav_actionlink' => $topnav_actionlink, 'topdrop' => $topdrop, 'topfish' => $topfish), $site_width); ?><?php if ( $topnav_wrap == 1 ) { echo ' wrap-on'; } else { echo ' wrap-off'; } ?> primary-nav">
+	<?php if ( $topnav_inner == 1 ) { ?><div id="topnav-inner" class="clearer"><?php } ?>
 	  <?php if(JDocumentHTML::countModules('user3')) { ?>
 	  	<div id="nav" class="clearer">
 	     <jdoc:include type="modules" name="user3" style="<?php if( $debug_modules == 1 ){ echo 'outline'; } else { echo 'none'; } ?>" />
@@ -14,10 +14,11 @@
 	     <jdoc:include type="modules" name="user4" style="<?php if( $debug_modules == 1 ){ echo 'outline'; } else { echo 'none'; } ?>" />
 	    </div>
 	  <?php } ?>
-	  
-	</div>
+	<?php if ( $topnav_inner == 1 ) { ?></div><?php } ?>
+	  </div>
+
     <?php if ( $topnav_wrap == 1 ) { ?></div><?php } ?>
     <?php if ( $topdrop >= 1 ) { ?>
-    	<div id="topdrop-bar-wrap"></div>
+    	<div id="topdrop-bar-wrap"  class="topnav <?php if ( $topnav_wrap == 0 ) {  echo pt_classes(array(), $site_width); } ?>"></div>
     <?php } ?>
 <?php } ?>
