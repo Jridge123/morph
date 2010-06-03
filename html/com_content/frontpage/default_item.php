98f5f86fd3fd8918b63695e4a7b5fdbafdc78db8
Chris Rault <Chris@Chris-Raults-iMac.local>
2010-06-03 10:21:51 +0200

    fixed the article preview conditional wrap.

--- html/com_content/frontpage/default_item.php
+++ html/com_content/frontpage/default_item.php
@@ -2,7 +2,7 @@
 defined('_JEXEC') or die('Restricted access');
 include_once(dirname(__FILE__).'/../icon.php');
 $canEdit	= ($this->user->authorize('com_content', 'edit', 'content', 'all') || $this->user->authorize('com_content', 'edit', 'content', 'own'));
-$morph = Morph::getInstance()
+$morph = Morph::getInstance();
 ?>
 <?php if ($this->item->state == 0) : ?>
 <div class="system-unpublished">
@@ -84,8 +84,8 @@ $morph = Morph::getInstance()
     <?php if ($this->item->params->get('show_readmore') && $this->item->readmore) : ?>
     <p class="readon">    
     <?php if ($morph->article_preview) : ?>
-    <a href="<?php echo $this->item->readmore_link; ?>" title="<?php echo JText::sprintf($this->item->title); ?>" rel="article-preview" class="preview-link preview-disabled"><?php echo JText::_('Preview'); ?></a>
-	<?php endif ?>
+    <a href="<?php echo $this->item->readmore_link; ?>" title="<?php echo JText::sprintf($this->item->title); ?>" rel="article-preview" class="preview-link"><?php echo JText::_('Preview'); ?></a>
+    <?php endif; ?>
 	<a href="<?php echo $this->item->readmore_link; ?>" title="<?php echo JText::sprintf($this->item->title); ?>">
 	<?php if ($this->item->readmore_register) :
 		echo JText::_('Register to read more...');
