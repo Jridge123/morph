dbe99da9a2f82d31e285c81df24dc40629386187
Chris Rault <Chris@Chris-Raults-iMac.local>
2010-07-02 14:47:19 +0200

    Added some missing arrays for the new extra shelves.

--- core/morphArrays.php
+++ core/morphArrays.php
@@ -3,6 +3,6 @@
 // javascript arrays
 $js_jquery = array($jquery_core, (bool)$tabscount, (bool)$accordionscount, $lazyload_enabled, $captions_enabled, $lightbox_enabled, $fontsizer_enabled, $shareit_enabled);
 $js_jqueryui = array((bool)$tabscount, (bool)$accordionscount);
-$js_cookie = array((bool)$tabscount, (bool)$accordionscount, $toolbar_slider, $topshelf_slider, $bottomshelf_slider, $developer_toolbar);
-$js_slider = array($toolbar_slider, $topshelf_slider, $bottomshelf_slider);
-$js_equalize = array($toolbar_equalize, $masthead_equalize, $subhead_equalize, $topnav_equalize, $topshelf_equalize, $bottomshelf_equalize, $user1_equalize, $user2_equalize, $inset1_equalize, $inset2_equalize, $inset3_equalize, $inset4_equalize, $outer1_equalize, $outer2_equalize, $outer3_equalize, $outer4_equalize, $outer5_equalize, $inner1_equalize, $inner2_equalize, $inner3_equalize, $inner4_equalize, $inner5_equalize, $footer_equalize);
+$js_cookie = array((bool)$tabscount, (bool)$accordionscount, $toolbar_slider, $topshelf_slider, $bottomshelf_slider, $bottomshelf2_slider, $bottomshelf3_slider, $developer_toolbar);
+$js_slider = array($toolbar_slider, $topshelf_slider, $bottomshelf_slider, $bottomshelf2_slider, $bottomshelf3_slider);
+$js_equalize = array($toolbar_equalize, $masthead_equalize, $subhead_equalize, $topnav_equalize, $topshelf_equalize, $bottomshelf_equalize, $bottomshelf2_equalize, $bottomshelf3_equalize, $user1_equalize, $user2_equalize, $inset1_equalize, $inset2_equalize, $inset3_equalize, $inset4_equalize, $outer1_equalize, $outer2_equalize, $outer3_equalize, $outer4_equalize, $outer5_equalize, $inner1_equalize, $inner2_equalize, $inner3_equalize, $inner4_equalize, $inner5_equalize, $footer_equalize);
