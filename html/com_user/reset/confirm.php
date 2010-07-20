2dbdd6686f0ce28e0afc6daa9333554891aaebc2
Chris Rault <Chris@Chris-Raults-iMac.local>
2010-07-20 11:14:31 +0200

    Added a fix for the invalid token bug in com_user.

--- html/com_user/reset/confirm.php
+++ html/com_user/reset/confirm.php
@@ -4,11 +4,17 @@
 <p class="login-description"><?php echo JText::_('RESET_PASSWORD_CONFIRM_DESCRIPTION'); ?></p>
 <form action="<?php echo JRoute::_( 'index.php?option=com_user&amp;task=confirmreset' ); ?>" method="post" class="josForm form-validate login-confirm" id="login-form">
 	<ul>
-		<li><label for="token" class="hasTip" title="<?php echo JText::_('RESET_PASSWORD_TOKEN_TIP_TITLE'); ?>::<?php echo JText::_('RESET_PASSWORD_TOKEN_TIP_TEXT'); ?>"><?php echo JText::_('Token'); ?>:</label>
+		<li>
+			<label for="username" class="hasTip" title="<?php echo JText::_('RESET_PASSWORD_USERNAME_TIP_TITLE'); ?>::<?php echo JText::_('RESET_PASSWORD_USERNAME_TIP_TEXT'); ?>"><?php echo JText::_('User Name'); ?>:</label>
+			<input id="username" name="username" type="text" class="required form-input" size="36" />
 		</li>
-		<li><input id="token" name="token" type="text" class="required form-input" size="36" /></li>
-
-		<li class="login-btn"><button type="submit" class="validate"><?php echo JText::_('Submit'); ?></button></li>
+		<li>
+			<label for="token" class="hasTip" title="<?php echo JText::_('RESET_PASSWORD_TOKEN_TIP_TITLE'); ?>::<?php echo JText::_('RESET_PASSWORD_TOKEN_TIP_TEXT'); ?>"><?php echo JText::_('Token'); ?>:</label>
+			<input id="token" name="token" type="text" class="required form-input" size="36" />
+		</li>
+		<li class="login-btn">
+			<button type="submit" class="validate"><?php echo JText::_('Submit'); ?></button>
+		</li>
 	</ul>
 	<?php echo JHTML::_( 'form.token' ); ?>
 	</form>
