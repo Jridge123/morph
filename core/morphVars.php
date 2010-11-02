d1e8b39ae9d882d5ddfcd4eff0d1d9164be27465
Chris Rault <Chris@Chris-Raults-iMac.local>
2010-11-02 13:25:58 +0200

    Updated the menu db checks with the new "masthead" position that replaces "top".

--- core/morphVars.php
+++ core/morphVars.php
@@ -11,13 +11,13 @@ $db->setQuery( $query ); $accordionscount = $db->loadResult();
 $query = "SELECT COUNT(*) FROM `#__modules` WHERE `params` LIKE '%moduleclass_sfx=rounded%' ";
 $db->setQuery( $query ); $roundedcount = $db->loadResult();
 
-$query = "SELECT COUNT(*) FROM `#__modules` WHERE `position` = 'user3'  AND published = '1' AND `params` LIKE '%moduleclass_sfx=topdrop%' OR `position` = 'user3' AND published = '1' AND `params` LIKE '% topdrop%' OR `position` = 'top' AND published = '1' AND `params` LIKE '%moduleclass_sfx=topdrop%' OR `position` = 'top' AND published = '1' AND `params` LIKE '% topdrop%'";
+$query = "SELECT COUNT(*) FROM `#__modules` WHERE `position` = 'user3'  AND published = '1' AND `params` LIKE '%moduleclass_sfx=topdrop%' OR `position` = 'user3' AND published = '1' AND `params` LIKE '% topdrop%' OR `position` = 'masthead' AND published = '1' AND `params` LIKE '%moduleclass_sfx=topdrop%' OR `position` = 'masthead' AND published = '1' AND `params` LIKE '% topdrop%'";
 $db->setQuery( $query ); $topdrop = $db->loadResult();
 
-$query = "SELECT COUNT(*) FROM `#__modules` WHERE `position` = 'user3' AND published = '1' AND `params` LIKE '%moduleclass_sfx=topfish%' OR `position` = 'user3' AND published = '1' AND `params` LIKE '% topfish%' OR `position` = 'top' AND published = '1' AND `params` LIKE '%moduleclass_sfx=topfish%' OR `position` = 'top' AND published = '1' AND `params` LIKE '% topfish%'";
+$query = "SELECT COUNT(*) FROM `#__modules` WHERE `position` = 'user3' AND published = '1' AND `params` LIKE '%moduleclass_sfx=topfish%' OR `position` = 'user3' AND published = '1' AND `params` LIKE '% topfish%' OR `position` = 'masthead' AND published = '1' AND `params` LIKE '%moduleclass_sfx=topfish%' OR `position` = 'masthead' AND published = '1' AND `params` LIKE '% topfish%'";
 $db->setQuery( $query ); $topfish = $db->loadResult();
 
-$query = "SELECT COUNT(*) FROM `#__modules` WHERE `position` = 'user3' AND published = '1' AND `params` LIKE '%moduleclass_sfx=subtext%' OR `position` = 'user3' AND published = '1' AND `params` LIKE '% subtext%' OR `position` = 'top' AND published = '1' AND `params` LIKE '%moduleclass_sfx=subtext%' OR `position` = 'top' AND published = '1' AND `params` LIKE '% subtext%'";
+$query = "SELECT COUNT(*) FROM `#__modules` WHERE `position` = 'user3' AND published = '1' AND `params` LIKE '%moduleclass_sfx=subtext%' OR `position` = 'user3' AND published = '1' AND `params` LIKE '% subtext%' OR `position` = 'masthead' AND published = '1' AND `params` LIKE '%moduleclass_sfx=subtext%' OR `position` = 'masthead' AND published = '1' AND `params` LIKE '% subtext%'";
 $db->setQuery( $query ); $subtext = $db->loadResult();
 
 $query = "SELECT COUNT(*) FROM `#__modules` WHERE `position` = 'user3' AND `params` LIKE '%moduleclass_sfx=animate%' OR `position` = 'user3' AND `params` LIKE '% animate%'";
@@ -29,7 +29,7 @@ $db->setQuery( $query ); $sidefish = $db->loadResult();
 $query = "SELECT COUNT(*) FROM `#__modules` WHERE `position` = 'outersplit' AND `module` = 'mod_mainmenu' OR `position` = 'outer1' AND `module` = 'mod_mainmenu' OR `position` = 'outer2' AND `module` = 'mod_mainmenu' OR `position` = 'outer3' AND `module` = 'mod_mainmenu' OR `position` = 'outer4' AND `module` = 'mod_mainmenu' OR `position` = 'outer5' AND `module` = 'mod_mainmenu' OR `position` = 'innersplit' AND `module` = 'mod_mainmenu' OR `position` = 'inner1' AND `module` = 'mod_mainmenu' OR `position` = 'inner2' AND `module` = 'mod_mainmenu' OR `position` = 'inner3' AND `module` = 'mod_mainmenu' OR `position` = 'inner4' AND `module` = 'mod_mainmenu' OR `position` = 'inner5' AND `module` = 'mod_mainmenu'" ;
 $db->setQuery( $query ); $sidenav_count = $db->loadResult();
 
-$query = "SELECT COUNT(*) FROM `#__modules` WHERE `position` = 'user3' AND `module` = 'mod_mainmenu' OR `position` = 'top' AND `module` = 'mod_mainmenu'  OR `position` = 'toolbar' AND `module` = 'mod_mainmenu'";
+$query = "SELECT COUNT(*) FROM `#__modules` WHERE `position` = 'user3' AND `module` = 'mod_mainmenu' OR `position` = 'masthead' AND `module` = 'mod_mainmenu'  OR `position` = 'toolbar' AND `module` = 'mod_mainmenu'";
 $db->setQuery( $query ); $topnav_count = $db->loadResult();
 
 $query = "SELECT COUNT(*) FROM `#__modules` WHERE `module` = 'mod_simpleticker' AND `published` = '1'";
@@ -44,7 +44,7 @@ $db->setQuery( $query ); $simplecontact = $db->loadResult();
 $query = "SELECT COUNT(*) FROM `#__modules` WHERE `module` = 'mod_simplesocial' AND `published` = '1'";
 $db->setQuery( $query ); $simplesocial = $db->loadResult();
 
-//Let's pass session variables to the js and css views so we only have to run the sql queries once.
+// Let's pass session variables to the js and css views so we only have to run the sql queries once.
 $counts = array('tabscount', 'accordionscount', 'roundedcount', 'topdrop', 'topfish', 'subtext', 'animate_top', 'sidefish', 'sidenav_count', 'topnav_count', 'simpleticker', 'simpletweet', 'simplecontact', 'simplesocial');
 foreach($counts as $count)
 {
