391248ff56e489332343e94378f10c853f3cb393
Chris Rault <Chris@Chris-Raults-iMac.local>
2010-11-05 14:26:21 +0200

    - Updated the block class include to work in Morph or via the themelet include override.

--- core/includes/bottomshelf1.php
+++ core/includes/bottomshelf1.php
@@ -1,6 +1,6 @@
 <?php defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
 <?php if($global_wrap == 1 && $global_wrap_start == 4){ ?><div id="global-wrap" class="<?php echo $site_width; ?>"><?php } ?>
 <?php 
-$position = 'bottomshelf3'; 
+$position = 'bottomshelf1'; 
 include ($blockclassespath); echo blocks($position, $this, $jj_const, $classes, '', $debug_modules, $nojs);
-if($global_wrap == 1 && $global_wrap_end == 3){ ?></div><?php } ?>
+if($global_wrap == 1 && $global_wrap_end == 1){ ?></div><?php } ?>
--- core/includes/bottomshelf2.php
+++ core/includes/bottomshelf2.php
@@ -1,6 +1,6 @@
-<?php defined( '_JEXEC' ) or die( 'Restricted access' );
-$position = 'bottomshelf2';
-include dirname(__FILE__).'/../morphBlockClasses.php';
-echo blocks($position, $this, $jj_const, $classes, $site_width, $debug_modules, $nojs);
-?>
-<?php if($global_wrap_end == 1){ ?></div><?php } ?>
+<?php defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
+<?php if($global_wrap == 1 && $global_wrap_start == 4){ ?><div id="global-wrap" class="<?php echo $site_width; ?>"><?php } ?>
+<?php 
+$position = 'bottomshelf2'; 
+include ($blockclassespath); echo blocks($position, $this, $jj_const, $classes, '', $debug_modules, $nojs);
+if($global_wrap == 1 && $global_wrap_end == 2){ ?></div><?php } ?>
