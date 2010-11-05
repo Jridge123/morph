3b67279f0c8d8c5972562749b434e3194690b64c
Chris Rault <Chris@Chris-Raults-iMac.local>
2010-11-05 14:24:26 +0200

    - Renamed the top and bottomshelf includes to be consistent with the other shelves.

--- core/includes/bottomshelf.php
+++
@@ -1,6 +0,0 @@
-<?php defined( '_JEXEC' ) or die( 'Restricted access' );
-$position = 'bottomshelf';
-include dirname(__FILE__).'/../morphBlockClasses.php';
-echo blocks($position, $this, $jj_const, $classes, $site_width, $debug_modules, $nojs);
-?>
-<?php if($global_wrap_end == 1){ ?></div><?php } ?>
--- core/includes/topshelf.php
+++
@@ -1,6 +0,0 @@
-<?php defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
-<?php if($global_wrap == 1 && $global_wrap_start == 4){ ?><div id="global-wrap" class="<?php echo $site_width; ?>"><?php } ?>
-<?php 
-$position = 'topshelf';
-include dirname(__FILE__).'/../morphBlockClasses.php';
-echo blocks($position, $this, $jj_const, $classes, $site_width, $debug_modules, $nojs);
