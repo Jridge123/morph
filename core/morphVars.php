d27fcff34547a55dffcf4ddd6f695d2b11a6b31c
Chris Rault <Chris@Chris-Raults-iMac.local>
2010-11-09 13:36:40 +0200

    - Replaced a number of sql queries with Joomla's Module Helper api.

--- core/morphVars.php
+++ core/morphVars.php
@@ -1,6 +1,8 @@
 <?php defined( '_JEXEC' ) or die( 'Restricted access' );
 
 $db=& JFactory::getDBO();
+$doc =& JFactory::getDocument();
+jimport('joomla.application.module.helper');
 
 $query = "SELECT COUNT(*) FROM `#__configurator` WHERE `param_value` = 'tabs' ";
 $db->setQuery( $query ); $tabscount = $db->loadResult();
@@ -32,17 +34,11 @@ $db->setQuery( $query ); $sidenav_count = $db->loadResult();
 $query = "SELECT COUNT(*) FROM `#__modules` WHERE `position` = 'user3' AND `module` = 'mod_mainmenu' OR `position` = 'masthead' AND `module` = 'mod_mainmenu'  OR `position` = 'toolbar' AND `module` = 'mod_mainmenu'";
 $db->setQuery( $query ); $topnav_count = $db->loadResult();
 
-$query = "SELECT COUNT(*) FROM `#__modules` WHERE `module` = 'mod_simpleticker' AND `published` = '1'";
-$db->setQuery( $query ); $simpleticker = $db->loadResult();
-
-$query = "SELECT COUNT(*) FROM `#__modules` WHERE `module` = 'mod_simpletweet' AND `published` = '1'";
-$db->setQuery( $query ); $simpletweet = $db->loadResult();
-
-$query = "SELECT COUNT(*) FROM `#__modules` WHERE `module` = 'mod_simplecontact' AND `published` = '1'";
-$db->setQuery( $query ); $simplecontact = $db->loadResult();
-
-$query = "SELECT COUNT(*) FROM `#__modules` WHERE `module` = 'mod_simplesocial' AND `published` = '1'";
-$db->setQuery( $query ); $simplesocial = $db->loadResult();
+$simpleticker = JModuleHelper::isEnabled( 'simpletweet' );
+$simpletweet = JModuleHelper::isEnabled( 'simpletweet' );
+$simplecontact = JModuleHelper::isEnabled( 'simpletweet' );
+$simplecontact = JModuleHelper::isEnabled( 'simplesocial' );
+$simplesocial = JModuleHelper::isEnabled( 'simplesocial' );
 
 // Let's pass session variables to the js and css views so we only have to run the sql queries once.
 $counts = array('tabscount', 'accordionscount', 'roundedcount', 'topdrop', 'topfish', 'subtext', 'animate_top', 'sidefish', 'sidenav_count', 'topnav_count', 'simpleticker', 'simpletweet', 'simplecontact', 'simplesocial');
