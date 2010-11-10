fb0e4aa5c82f49fd7b0982010b5c8d7a7d53a495
Chris Rault <Chris@Chris-Raults-iMac.local>
2010-11-10 13:05:37 +0200

    uncommented the hooks stuff until tomorrow, when Andy can take a look.

--- core/includes/bottomshelf1.php
+++ core/includes/bottomshelf1.php
@@ -1,8 +1,8 @@
 <?php defined( '_JEXEC' ) or die( 'Restricted access' );
 $position = 'bottomshelf1'; 
-do_action('bottomshelf1_before');
+//do_action('bottomshelf1_before');
 include ($blockclassespath); 
 echo blocks($position, $this, $jj_const, $classes, $site_width, $debug_modules); 
 if($global_wrap == 1 && $global_wrap_end == 1){ echo '</div>'; }
-do_action('bottomshelf1_after');
+//do_action('bottomshelf1_after');
 ?>
