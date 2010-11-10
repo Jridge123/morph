2d534f26c5917f7e96216c7f487b22a3c0f4e7a6
Chris Rault <Chris@Chris-Raults-iMac.local>
2010-11-10 10:19:48 +0200

    - Fixed the bottomshelves not getting the site width class.

--- core/includes/bottomshelf1.php
+++ core/includes/bottomshelf1.php
@@ -1,7 +1,7 @@
 <?php defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
-<?php if($global_wrap == 1 && $global_wrap_start == 4){ ?><div id="global-wrap" class="<?php echo $site_width; ?>"><?php } ?>
 <?php 
 $position = 'bottomshelf1'; 
 include ($blockclassespath); 
-echo blocks($position, $this, $jj_const, $classes, '', $debug_modules, $nojs);
-if($global_wrap == 1 && $global_wrap_end == 1){ ?></div><?php } ?>
+echo blocks($position, $this, $jj_const, $classes, $site_width, $debug_modules); 
+if($global_wrap == 1 && $global_wrap_end == 1){ echo '</div>'; }
+?>
