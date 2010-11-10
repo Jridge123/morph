ede3798f77613d32b3922f7a7643bbbc92d144d9
Chris Rault <Chris@Chris-Raults-iMac.local>
2010-11-10 14:10:14 +0200

    Fixed the iphone footer controls showing up in the desktop version.

--- core/includes/iphone_footer.php
+++ core/includes/iphone_footer.php
@@ -1,10 +1,12 @@
-<?php defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
-<?php $this->addScript(JURI::root().$templatepath .'/core/js/iphone_switch.js') ?>
-<?php $switch = isset($_COOKIE['iPhone']) ? $_COOKIE['iPhone'] : 'optimized' ?>
-<?php $swichActive = array($switch == 'optimized' ? 'active-mode' : '', $switch == 'normal' ? 'active-mode' : '') ?>
-<div id="switch">
-    <ul>
-        <li class="optimized <?php echo $swichActive[0] ?>"><a href="#switch-optimized" onclick="switchState()">Optimized mode</a></li>
-        <li class="normal <?php echo $swichActive[1] ?>"><a href="#switch-normal" onclick="switchState()">Normal mode</a></li>
-    </ul>
-</div>
+<?php defined( '_JEXEC' ) or die( 'Restricted access' );
+if(file_exists($inc_iphonefooter)) { include_once($inc_iphonefooter); } else {  ?>
+	<?php $this->addScript(JURI::root().$templatepath .'/core/js/iphone_switch.js') ?>
+	<?php $switch = isset($_COOKIE['iPhone']) ? $_COOKIE['iPhone'] : 'optimized' ?>
+	<?php $swichActive = array($switch == 'optimized' ? 'active-mode' : '', $switch == 'normal' ? 'active-mode' : '') ?>
+	<div id="switch">
+	    <ul>
+	        <li class="optimized <?php echo $swichActive[0] ?>"><a href="#switch-optimized" onclick="switchState()">Optimized mode</a></li>
+	        <li class="normal <?php echo $swichActive[1] ?>"><a href="#switch-normal" onclick="switchState()">Normal mode</a></li>
+	    </ul>
+	</div>
+<?php } ?>
