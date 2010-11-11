7c48fa8eaea879a22a02c86caa7fa5279ea26c4c
Chris Rault <Chris@Chris-Raults-iMac.local>
2010-11-11 20:15:32 +0200

    fixed the inconsistent hook names

--- core/includes/toolbar.php
+++ core/includes/toolbar.php
@@ -1,8 +1,8 @@
 <?php defined( '_JEXEC' ) or die( 'Restricted access' );
 if($global_wrap == 1 && $global_wrap_end == 0){ echo '<div id="global-wrap" class="'.$site_width.'">'; }
 $position = 'toolbar'; 
-$action->do_action('toolbar_before');
+$action->do_action ('toolbar_before');
 include ($blockclassespath); 
 echo blocks($position, $this, $jj_const, $classes, $site_width, $debug_modules); 
-$action->do_action('toolbar_after');
+$action->do_action ('toolbar_after');
 ?>
