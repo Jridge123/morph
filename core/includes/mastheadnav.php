ec8e7d6135be631dcfc9caaf8e33f35fcf329be9
Chris <chris@prothemer.com>
2010-11-30 10:09:31 +0200

    Added missing Morph prefix.

--- core/includes/mastheadnav.php
+++ core/includes/mastheadnav.php
@@ -2,7 +2,7 @@
 $morph = Morph::getInstance();
 if(file_exists($inc_mastheadnav)) { include_once($inc_mastheadnav); } else { ?>
 	<?php if(Morph::countModules('user3')) { ?>
-	<div id="topnav" class="mastheadnav <?php echo $morph->topnav_blockfx.' '; echo pt_classes(array('subtext' => $morph->subtext, 'topnav_actionlink' => $topnav_actionlink, 'topdrop' => $morph->topdrop, 'topfish' => $morph->topfish)); ?>">
+	<div id="topnav" class="mastheadnav <?php echo $morph->topnav_blockfx.' '; echo pt_classes(array('subtext' => $morph->subtext, 'topnav_actionlink' => $morph->topnav_actionlink, 'topdrop' => $morph->topdrop, 'topfish' => $morph->topfish)); ?>">
 		<div id="nav" class="clearer">
 			<jdoc:include type="modules" name="user3" style="<?php if( $morph->debug_modules == 1 ){ echo 'outline'; } else { echo 'none'; } ?>" />
 		</div>
